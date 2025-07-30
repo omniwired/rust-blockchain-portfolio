fa494d560a4cd580436635c3598f8205f24474e1
Eng. Juan Combetto <omniwired@gmail.com>
2025-07-30 15:57:38 +0900

    feat(zk-circuit): implement zk circuit

--- zk-circuit/src/lib.rs
+++ zk-circuit/src/lib.rs
@@ -1,34 +1,30 @@
 use ark_bn254::{Bn254, Fr};
 use ark_groth16::{Groth16, Proof, ProvingKey, VerifyingKey};
+use ark_r1cs_std::fields::fp::FpVar;
 use ark_relations::r1cs::{ConstraintSynthesizer, ConstraintSystemRef, SynthesisError};
 use ark_r1cs_std::prelude::*;
-use ark_std::rand::{RngCore, CryptoRng};
 use ark_snark::SNARK;
+use ark_std::rand::{CryptoRng, RngCore};
 
 #[derive(Clone)]
-pub struct HeaderVerificationCircuit {
+pub struct AddCircuit {
     // Public inputs
-    pub header_hash: Option<[u8; 32]>,
-    pub app_hash: Option<[u8; 32]>,
-    
-    // Private witness (the actual verification logic would go here)
-    pub signatures_valid: Option<bool>,
+    pub a: Option<Fr>,
+    pub c: Option<Fr>,
+
+    // Private witness
+    pub b: Option<Fr>,
 }
 
-impl ConstraintSynthesizer<Fr> for HeaderVerificationCircuit {
+impl ConstraintSynthesizer<Fr> for AddCircuit {
     fn generate_constraints(self, cs: ConstraintSystemRef<Fr>) -> Result<(), SynthesisError> {
-        // Allocate public inputs
-        let _header_hash_var = UInt8::new_input_vec(cs.clone(), &self.header_hash.unwrap_or([0u8; 32]))?;
-        let _app_hash_var = UInt8::new_input_vec(cs.clone(), &self.app_hash.unwrap_or([0u8; 32]))?;
-        
-        // Allocate private witness
-        let signatures_valid_var = Boolean::new_witness(cs.clone(), || {
-            Ok(self.signatures_valid.unwrap_or(false))
-        })?;
-        
-        // Constraint: signatures must be valid
-        signatures_valid_var.enforce_equal(&Boolean::TRUE)?;
-        
+        let a_var = FpVar::new_input(cs.clone(), || self.a.ok_or(SynthesisError::AssignmentMissing))?;
+        let b_var = FpVar::new_witness(cs.clone(), || self.b.ok_or(SynthesisError::AssignmentMissing))?;
+        let c_var = FpVar::new_input(cs.clone(), || self.c.ok_or(SynthesisError::AssignmentMissing))?;
+
+        let sum = a_var + b_var;
+        c_var.enforce_equal(&sum)?;
+
         Ok(())
     }
 }
@@ -40,40 +36,72 @@ pub struct ZkProver {
 
 impl ZkProver {
     pub fn setup<R: RngCore + CryptoRng>(mut rng: R) -> anyhow::Result<Self> {
-        let circuit = HeaderVerificationCircuit {
-            header_hash: None,
-            app_hash: None,
-            signatures_valid: None,
+        let circuit = AddCircuit {
+            a: None,
+            b: None,
+            c: None,
         };
-        
+
         let (pk, vk) = Groth16::<Bn254>::circuit_specific_setup(circuit, &mut rng)
             .map_err(|e| anyhow::anyhow!("Setup failed: {:?}", e))?;
-            
+
         Ok(ZkProver {
             proving_key: pk,
             verifying_key: vk,
         })
     }
-    
+
     pub fn prove<R: RngCore + CryptoRng>(
         &self,
-        header_hash: [u8; 32],
-        app_hash: [u8; 32],
+        a: Fr,
+        b: Fr,
         mut rng: R,
-    ) -> anyhow::Result<Proof<Bn254>> {
-        let circuit = HeaderVerificationCircuit {
-            header_hash: Some(header_hash),
-            app_hash: Some(app_hash),
-            signatures_valid: Some(true), // In real impl, this would be verified
+    ) -> anyhow::Result<(Proof<Bn254>, Fr)> {
+        let c = a + b;
+
+        let circuit = AddCircuit {
+            a: Some(a),
+            b: Some(b),
+            c: Some(c),
         };
-        
+
         let proof = Groth16::<Bn254>::prove(&self.proving_key, circuit, &mut rng)
             .map_err(|e| anyhow::anyhow!("Proving failed: {:?}", e))?;
-            
-        Ok(proof)
+
+        Ok((proof, c))
     }
-    
+
     pub fn verifying_key(&self) -> &VerifyingKey<Bn254> {
         &self.verifying_key
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ark_std::rand::SeedableRng;
+    use ark_std::UniformRand;
+
+    #[test]
+    fn test_zk_circuit_setup_and_prove() {
+        let mut rng = ark_std::rand::rngs::StdRng::seed_from_u64(42);
+
+        // Setup
+        let prover_result = ZkProver::setup(&mut rng);
+        assert!(prover_result.is_ok(), "Setup should succeed");
+        let prover = prover_result.unwrap();
+
+        // Prove
+        let a = Fr::rand(&mut rng);
+        let b = Fr::rand(&mut rng);
+        let (proof, c) = prover.prove(a, b, &mut rng).unwrap();
+
+        use ark_groth16::Groth16;
+        use ark_snark::SNARK;
+
+        let pvk = Groth16::process_vk(&prover.verifying_key).unwrap();
+        let valid_proof = Groth16::verify_with_processed_vk(&pvk, &[a, c], &proof).unwrap();
+
+        assert!(valid_proof, "Proof verification should succeed");
     }
 }
